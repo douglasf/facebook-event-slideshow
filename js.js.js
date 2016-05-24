f949e91cb2c36477d0a4369ab4def37f9055558f
Douglas Frisk <douglasfrisk@gmail.com>
2016-05-24 22:55:20 +0200

    API test

--- js.js.js
+++ js.js.js
@@ -3,6 +3,10 @@
   $('#play').on('click', function() {
     $('body').addClass('playing');
     $('input').attr('disabled', 'disabled');
+    $.get('https://api.instagram.com/v1/tags/snowy/media/recent?client_id=31c7a58c9b2d44aaa996be3704daaea3&callback=foo');
+    var foo = function(data) {
+      console.log(data);
+    };
   });
 
   $(window).on('keydown', function(e) {
