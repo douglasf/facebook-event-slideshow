fc9a94a6e941d0888353456e54330c37fa28ffe5
Douglas Frisk <douglasfrisk@gmail.com>
2016-05-25 00:21:30 +0200

    API test

--- js.js.js
+++ js.js.js
@@ -2,13 +2,14 @@
   var access_token, update_timeout, reload_timeout, images, images_i;
 
   var update = function() {
+    clearInterval(reload_timeout);
     $.ajax({
       url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + access_token + '&callback=foo',
       type: 'GET',
       crossDomain: true,
       dataType: 'jsonp',
       success: function(data) {
-        console.log(data.data);
+        console.log("New data loaded");
         $('body').addClass('loaded');
         images = data.data;
         images_i = images.length;
@@ -39,6 +40,7 @@
   $(window).on('keydown', function(e) {
     if (e.which === 27) {
       $('body').removeClass('playing');
+      $('body').removeAttr('style');
       $('input').removeAttr('disabled');
       clearInterval(update_timeout);
       clearInterval(reload_timeout);
